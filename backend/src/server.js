3ce9d9f719dcdcfcc670fa7df27b8752871e70df
Diego Prandi Silva <diegoprandi45@gmail.com>
2026-09-19 09:59:13 -0300

    feat: config inicial no servidor node

---
+++ backend/src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/teste', (req, res) => {
+    res.json({
+        mensagem: 'opann',
+    });
+});
+
+app.get('/', (req, res) => {
+    res.send('Servidor funfando!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor BACKEND rodando em http://localhost:${PORT}`);
+});
